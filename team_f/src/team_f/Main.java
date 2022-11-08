85dddc1451746b0572fc6a5020751b231ea42d33
82109 <82109@DESKTOP-DPBN850>
2022-11-08 13:02:22 +0900

    최태양 추가

--- team_f/src/team_f/Main.java
+++ team_f/src/team_f/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("189142조유송");
+		System.out.println("189142議곗쑀�넚");
+		System.out.println("213627최태양");
 
 	}
 
