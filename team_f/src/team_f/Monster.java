1661114d05456be124fd5c2fcef3ff3ef120677c
yuson <yuson@192.168.219.100>
2022-11-17 22:20:51 +0900

    길 x,y값 몹 데이터 기초

--- team_f/src/team_f/Monster.java
+++
@@ -1,5 +0,0 @@
-package team_f;
-
-public class Monster {
-
-}
--- team_f/src/team_f/Stage.java
+++ team_f/src/team_f/Stage.java
@@ -11,10 +11,10 @@ public class Stage {
 	int setting = 0;
 	int[][] mapdata = new int[16][24]; //흙 위치- 게임 재시작하거나 맵 변경시 다시 0으로 초기화할것
 	Screen screen;
+	public static int mobNumber;
 	public Stage(){
 	back	= new ImageIcon(getClass().getClassLoader().getResource("Resource/jandy-2.png")).getImage();
 	Load = new ImageIcon(getClass().getClassLoader().getResource("Resource/load.png")).getImage();
-	
 	}
 	// 맵 여러개니까 길 그리는 경로
 	public void Setting(int a) {
@@ -26,8 +26,9 @@ public class Stage {
 	}
 	// 길 그리기(흙) 클래스
 	public void map(Graphics g) {
-		if(setting==0) {//기본맵
-		for(int i=18; i<24;i++) {
+		if(setting==0) {//기본맵 
+		mobNumber=100;//몹 마릿수
+		for(int i=18; i<24;i++) {// 리스트에 1들어갈시 길
 			mapdata[7][i]=1;
 		}
 		mapdata[6][18]=1;
@@ -44,11 +45,16 @@ public class Stage {
 			mapdata[7][i]=1;
 		}
 		}
+		int[][] next=new int[29][2];// 객체의 갯수는 길의 갯수와 x,y이동좌표가 들어갈 것
 		//길 그리는 부분
+		int count=0;
 		for(int i =0; i<24;i++) {
 			for(int j=0;j<16;j++) {
 				if(mapdata[j][i]==1) {
-				g.drawImage(this.Load, i*30,j*30,screen);
+				int x=i*30, y=j*30;
+				next[count][0]= x;
+				next[count][1]=y;
+				g.drawImage(this.Load, x,y,screen); // 30은 load이미지의 크기
 				}
 			}
 		}
