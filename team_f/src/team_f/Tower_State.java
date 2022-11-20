545d8eafaffef5933049ac081f831b1df37f6ac5
yuson <yuson@192.168.219.101>
2022-11-20 23:01:21 +0900

    타워그리기 (좌표 미구현,마우스 이벤트 처리 필요)

--- team_f/src/team_f/Tower_State.java
+++ team_f/src/team_f/Tower_State.java
@@ -12,9 +12,9 @@ public class Tower_State {
 	public BufferedImage tower;
 	public int width = 24;
 	public int height = 24;
-	public int index_x = 0;//소스상의 사진위치 
+	public int index_x = 0; //애니매이션 n번째 동작
 	public int index_y = 0;
-	public int start_x = 0;//켄버스상의 그려지는 위치
+	public int start_x = 0;//소스상의 사진 위치
 	public int start_y = 0;
 	public int frame_size = 0;
 	public int Power=0;
