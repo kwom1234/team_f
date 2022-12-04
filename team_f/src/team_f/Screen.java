dac81a74afb6de3b0b8cffe670ac836931e9a2dc
yuson <yuson@DESKTOP-1GGKJSL>
2022-12-04 19:22:01 +0900

    몬스터 특정마릿수마다 교체

--- team_f/src/team_f/Screen.java
+++ team_f/src/team_f/Screen.java
@@ -16,11 +16,20 @@ public class Screen extends Canvas implements Runnable {
 	public static Image offscreen;
 	public static Graphics bufferGraphics;
 	Stage map = new Stage();
-	int Cmob1 = 40;
-	int count = 0;
-
+	int nextMobT = 40;
+	int NextC = 0;
+	int count=0;
 	Tower_Circle Tcircle = new Tower_Circle();
 	ArrayList<Monster_1> mob1 = new ArrayList<Monster_1>();
+	ArrayList<Monster_2> mob2 = new ArrayList<Monster_2>();
+	ArrayList<Monster_3> mob3 = new ArrayList<Monster_3>();
+	ArrayList<Monster_4> mob4 = new ArrayList<Monster_4>();
+	ArrayList<Monster_5> mob5 = new ArrayList<Monster_5>();
+	ArrayList<Monster_6> mob6 = new ArrayList<Monster_6>();
+	ArrayList<Monster_7> mob7 = new ArrayList<Monster_7>();
+	ArrayList<Monster_8> mob8 = new ArrayList<Monster_8>();
+	ArrayList<Monster_9> mob9 = new ArrayList<Monster_9>();
+	ArrayList<Monster_10> mob10 = new ArrayList<Monster_10>();
 
 	// ImageIcon background = new
 	// ImageIcon(getClass().getClassLoader().getResource("Resource/eraser.png"));
@@ -38,19 +47,51 @@ public class Screen extends Canvas implements Runnable {
 		}, 0, 1);
 		Timer MoveTimer = new Timer();
 		MoveTimer.schedule(new TimerTask() {
-		
 
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				for(Monster_1 i : mob1) {
-					i.move.MovePoint();	
+				for (Monster_1 i : mob1) {
+					i.move.MovePoint();
 				}
-				
-				count++;
-				if (count == Cmob1) {
-					mob1.add(new Monster_1());
-					count = 0;
+				for (Monster_2 i : mob2) {
+					i.move.MovePoint();
+				}
+				for (Monster_3 i : mob3) {
+					i.move.MovePoint();
+				}
+				for (Monster_4 i : mob4) {
+					i.move.MovePoint();
+				}
+				for (Monster_5 i : mob5) {
+					i.move.MovePoint();
+				}
+				for (Monster_6 i : mob6) {
+					i.move.MovePoint();
+				}
+				for (Monster_7 i : mob7) {
+					i.move.MovePoint();
+				}
+				for (Monster_8 i : mob8) {
+					i.move.MovePoint();
+				}
+				for (Monster_9 i : mob9) {
+					i.move.MovePoint();
+				}
+				for (Monster_10 i : mob10) {
+					i.move.MovePoint();
+				}
+				NextC++;
+				if (NextC == nextMobT) {
+					count++;
+					if(count<10) {
+						mob1.add(new Monster_1());	
+					}else if(count<20) {
+						mob2.add(new Monster_2());
+					}else if(count<30) {
+						mob3.add(new Monster_3());
+					}
+					NextC = 0;
 				}
 //				mob.move.MovePoint();
 
@@ -93,6 +134,33 @@ public class Screen extends Canvas implements Runnable {
 		map.background(g);
 		map.map(g);
 		for (Monster_1 i : mob1) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_2 i : mob2) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_3 i : mob3) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_4 i : mob4) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_5 i : mob5) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_6 i : mob6) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_7 i : mob7) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_8 i : mob8) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_9 i : mob9) {
+			i.drawMonster(g, this, i.move.Point);
+		}
+		for (Monster_10 i : mob10) {
 			i.drawMonster(g, this, i.move.Point);
 		}
 //		mob.drawMonster(g,this,mob.move.Point);
